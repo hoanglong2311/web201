15eec45ec7402b654155f265bef93d84abc92082
Hoang Long <70263014+hoanglong2311@users.noreply.github.com>
2021-08-14 07:23:06 -0700

    Add files via upload

---
+++ de_1/mycode.js
@@ -0,0 +1,11 @@
+function ChangeImg(img, number) {
+    if (number === 1) {
+        img.src = 'images/Miquang02.jpg';
+    } else if (number === 2){
+        img.src= 'images/spingroll02.jpg';
+    } else if (number === 3){
+        img.src='images/miquang.jpg';
+    } else if (number === 4){
+        img.src='images/spingroll.jpg'
+    }
+}
